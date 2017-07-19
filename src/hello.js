ce22064d980a528360a2535465a9cd7254062bc0
Flavie Loshouarn <Flavie Loshouarn@PVAL.com>
2017-07-19 15:08:18 +0200

    hello

---
+++ src/hello.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var recastai = require('recastai').default
+
+
+var connect = new recastai.connect('YOUR_REQUEST_TOKEN')
+
+var app = express()
+
+/* Server setup */
+app.set('port', 5000)
+app.use(bodyParser.json())
+app.post('/', function(req, res) {
+  connect.handleMessage(req, res, onMessage)
+})
+
+function onMessage (message) {
+  // Get the content of the message
+  var content = message.content
+
+  // Get the type of the message (text, picture,...)
+  var type = message.type
+
+  // Add a reply, and send it
+  message.addReply([{ type: 'text', content: 'Hello, world' }])
+  message.reply()
+}
